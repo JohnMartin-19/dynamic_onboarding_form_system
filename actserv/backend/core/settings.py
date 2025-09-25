97aefbb5fabf834a3b235e18ccb7b307ab83780b
John Mburu <johnnkonge2020@gmail.com>
2025-09-25 08:42:48 +0300

    added drf spectacular for API docs

--- actserv/backend/core/settings.py
+++ actserv/backend/core/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'corsheaders',
+    'rest_framework',
+    "drf_spectacular",
+    'rest_framework_simplejwt.token_blacklist',
     
     # "authentication.apps.AuthenticationConfig"
 
