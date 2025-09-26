2a75fc14e5b35be3cf12e32239d3f5600a1b5774
John Mburu <johnnkonge2020@gmail.com>
2025-09-26 14:50:26 +0300

    recreated the app

---
+++ actserv/backend/authentication/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = [
+        ('individual', 'Individual Client'),
+        ('business_owner', 'Business Owner'),
+        ('financial_advisor', 'Financial Advisor'),
+        ('accountant', 'Accountant'),
+        ('other', 'Other'),
+    ]
+
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True, null=True, blank=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    company_name = models.CharField(max_length=255, blank=True, null=True)
+    role = models.CharField(max_length=50, choices=ROLE_CHOICES)
+
+   
+
+    USERNAME_FIELD = 'username'  
+   
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
+
+    def __str__(self):
+        return f"{self.username} | {self.get_role_display()}"
---
+++ actserv/backend/forms/views.py
@@ -0,0 +1,159 @@
+from django.shortcuts import render
+from rest_framework.decorators import  permission_classes
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
+from rest_framework_simplejwt.tokens import RefreshToken,AccessToken
+from rest_framework_simplejwt.views import TokenObtainPairView
+from django.db import transaction
+from rest_framework.exceptions import ValidationError
+from django.shortcuts import get_object_or_404
+
+class FormCreateListAPIView(APIView):
+    """
+    FETCHING ALL FORMS 
+    """
+    serializer_class = FormSerializer
+    
+    def get(self, request):
+        forms = Form.objects.all()
+        serializer = self.serializer_class(forms, many = True)
+        return Response({'message':'Success','data':serializer.data},status=status.HTTP_200_OK)
+    
+    """
+    _Creating a FORM_
+
+    Args:
+        APIView (_POST_): _Lets the admin create a form _
+        
+    """
+    @permission_classes([IsAdminUser])
+    def post(self,request):
+        data = request.data
+        serializer = self.serializer_class(data = data)
+        if serializer.is_valid():
+            with transaction.atomic():
+                serializer.save()
+                return Response({'message':'Form created successfully','data':serializer.data},status = status.HTTP_201_CREATED)
+            return Response({'message':'Failed to create form', 'data':serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        
+class FormRetrieveUpdateDestroyAPIView(APIView):
+    """
+    helper method for getting a particular form by id
+    """
+    serializer_class = FormSerializer
+    def get_object(self, request,pk):
+        return get_object_or_404(pk=pk)
+    
+    def get(self, request,pk):
+        form = self.get_object(pk)
+        serializer = self.serializer_class(form)
+        return Response({'message':'Success', 'data':serializer.data}, status=status.HTTP_200_OK)
+    
+    def put(self, request, pk):
+        form = self.get_object(pk)
+        serializer = self.serializer_class(form, data = request.data, partial = True)
+        if serializer.is_valid():
+            with transaction.atomic():
+                serializer.save()
+                return Response({'message':'Form updated successfully','data':serializer.data}, status=status.HTTP_200_OK)
+            return Response({'mesage':'Failed to update form', 'data':serializer.errors},status=status.HTTP_400_BAD_REQUEST)
+        
+    def delete(self, pk):
+        form = self.get_object(pk)
+        form.delete()
+        return Response({'message':'Form delete successfully'},status=status.HTTP_204_NO_CONTENT)
+    
+class FieldCreateListAPIView(APIView):
+    """
+    API view to create and list all form fields
+    """
+    serializer_class = FieldSerializer
+    
+    def get(self, request):
+        fields = Field.objects.all()
+        serializer = self.serializer_class(fields, many=True)
+        return Response({'message':'Success','data':serializer.data}, status=status.HTTP_200_OK)
+    
+    @permission_classes([IsAdminUser])
+    def post(self, request):
+        data = request.data
+        serializer = self.serializer_class(data = data)
+        if serializer.is_valid():
+            with transaction.atomic():
+                serializer.save()
+                return Response({'message':'Field created successfully', 'data':serializer.data}, status=status.HTTP_201_CREATED)
+            return Response({'message':'Failed to create field', 'data':serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        
+class FieldRetrieveUpdateDestroyAPIView(APIView):
+    
+    serializer_class = FieldSerializer
+    
+    def get_object(self, request, pk):
+        return get_object_or_404(pk=pk)
+    
+    def get(self, request,pk):
+        field = self.get_object(pk)
+        serializer = self.serializer_class(field)
+        return Response({'message':'Success', 'data':serializer.data}, status = status.HTTP_200_OK)
+    
+    def put(self,request, pk):
+        field = self.get_object(pk)
+        serializer = self.serializer_class(field,data = request.data,partial = True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message':'Field updated successfully', 'data':serializer.data}, status = status.HTTP_200_OK)
+        return Response({'message':'Failed to update the field', 'data':serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, pk):
+        field = self.get_object(pk)
+        field.delete()
+        return Response({'message':'Field deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+    
+class SubmissionCreateListAPIView(APIView):
+    
+    serializer_class = SubmissionSerializer
+    permission_classes = [IsAuthenticated]
+   
+    def get(self, request):
+        submissions = Submission.objects.all()
+        serializer = self.serializer_class(submissions, many = True)
+        return Response({'message':'Success', 'data':serializer.data}, status=status.HTTP_200_OK)
+   
+    def post(self, request):
+        serializer = self.serializer_class(data = request.data)
+        if serializer.is_valid():
+            with transaction.atomic():
+                serializer.save()
+                return Response({'message':'Form submitted successfully', 'data':serializer.data}, status=status.HTTP_201_CREATED)
+            return Resposne({'message':'Failed to submit form', 'data':serializer.errors}, status= status.HTTP_400_BAD_REQUEST)
+        
+class SubmissionRetirieveUpdateDestroyAPIView(APIView):
+    
+    serializer_class = SubmissionSerializer
+    
+    def get_object(self, request, pk):
+        return get_object_or_404(pk=pk)
+    
+    def get(self, request, pk):
+        submission = self.get_object(pk)
+        serializer = self.serializer_class(submission)
+        return Response({'message':'Success', 'data':serializer.data}, status = status.HTTP_200_OK)
+    
+    def delete(self,pk):
+        submission = self.get_object(pk)
+        submission.delete()
+        return Response({'message':'Submission deleted successfully'}, status = status.HTTP_204_NO_CONTENT)
+        
+
+class MySubmissions(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = SubmissionSerializer
+    def get(self,request):
+        
+        my_submissions = Submission.objects.filter(user =request.user)
+        serializer = self.serializer_class(my_submissions)
+        return Response({'message':'Success','data':serializer.data}, status=status.HTTP_200_OK)
