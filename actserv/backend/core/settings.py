6b4ca3dca2158819e54eb1517d3a3948370fd6f6
John Mburu <johnnkonge2020@gmail.com>
2025-09-26 10:10:57 +0300

    handled CORS issues

--- actserv/backend/core/settings.py
+++ actserv/backend/core/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = 'django-insecure-$^1rjpv&##1q*bx=7kwt33gmxpshmhw$l)x25pn)zdrf3srjk(
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5174",  
+    "http://127.0.0.1:5174",
+    ]
+CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
 
@@ -51,6 +55,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
