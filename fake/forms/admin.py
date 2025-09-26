0b648fc9e9f3a574eaa1b7329c0b89a850f2e2f8
John Mburu <johnnkonge2020@gmail.com>
2025-09-26 14:55:31 +0300

    everything wokring as expected now

--- fake/forms/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(Form)
-admin.site.register(Field)
-admin.site.register(Submission)
-admin.site.register(Document)
